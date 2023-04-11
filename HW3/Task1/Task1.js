7b621a37e6cb22e17d4482087db53a5a0ad2c8f7
tim167 <tim167izk@gmail.com>
2023-04-11 17:06:28 +0300

    hw

---
+++ HW3/Task1/Task1.js
@@ -0,0 +1,7 @@
+let num1 = Number(prompt('Введіть перше число'));
+let num2 = Number(prompt('Введіть друге число'));
+let num3 = Number(prompt('Введіть третє число'));
+
+let result = (num1 + num2 + num3) / 3;
+
+alert(result)
