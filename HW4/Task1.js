ea7d585aeed65185a86dcb8d1ae2b2e351313a7e
tim167 <tim167izk@gmail.com>
2023-04-14 20:20:04 +0300

    hw4

--- HW4/Task1.js
+++ HW4/Task1.js
@@ -7,20 +7,5 @@ while(from <= to) {
   result.push(from);
   from += step;
 }
+
 console.log(result.join(' '));
-
-
-
-const num = prompt('Введіть число');
-
-let isPrime = num > 1;
-
-if (isPrime){  
-for(let i = 2; i < num; i++)
-  if(num % i === 0){
-    isPrime = false;
-    break;
-  }
-}
-
-console.log(isPrime);
